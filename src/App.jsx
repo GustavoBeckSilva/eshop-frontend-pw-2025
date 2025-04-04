5f8c12f2c386e1352b063e71d33a2ce6cb3b622e
GUSTAVO BECK DA SILVA <20231PF.CC0024@passofundo.ifsul.local>
2025-04-04 09:14:54 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@popperjs/core/dist/cjs/popper.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Menu from './componentes/telas/menu';
+import Home from './componentes/telas/home';
+import Sobre from './componentes/telas/sobre';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      },
+      {
+        path: "/sobre",
+        element: <Sobre/>
+      }
+    ]
+  }
+])
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/telas/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+
+    return(
+        <>
+            <h1>Seja bem vindo ao sistema eShop - PW</h1>
+        </>
+    )
+
+}
+
+export default Home;
